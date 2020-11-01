7d99f0dbd295cd83e4d160b0b637e2c0abe73359
Dadu <shaikzied878@gmail.com>
2020-11-01 15:56:58 -0800

    First commit

---
+++ src/List.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./List.css";
+
+const List = ({ people }) => {
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name} />
+            <h4>{name}</h4>
+            <p>{age}</p>
+          </article>
+        );
+      })}
+    </>
+  );
+};
+
+export default List;
---
+++ src/data.js
@@ -0,0 +1,86 @@
+export default [
+  {
+    id: 1,
+    name: "Shaik Dadapeer",
+    age: "8th March - 2004",
+    image:
+      "https://lh3.googleusercontent.com/ogw/ADGmqu8v14WJdYvU7byMUPLBwzJjD0vbuTIy5tKUBh9p=s83-c-mo",
+  },
+  {
+    id: 2,
+    name: "Shaik Mohammad Ghouse",
+    age: "12th September 2001",
+    image:
+      "https://scontent.fvtz3-1.fna.fbcdn.net/v/t1.0-9/61602368_877453799303611_758161439704743936_n.jpg?_nc_cat=106&ccb=2&_nc_sid=a4a2d7&_nc_ohc=6GvrZ2eFjpgAX_vbyNn&_nc_ht=scontent.fvtz3-1.fna&oh=2cd8bdd6eeb1a870240e6ae8aa3353d1&oe=5FC1742C",
+  },
+  {
+    id: 3,
+    name: "Yashwanth",
+    age: "25th Novemver 2003",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+  },
+  {
+    id: 4,
+    name: "Tauseef",
+    age: "8th June 2003",
+    image:
+      "https://scontent.fblr4-1.fna.fbcdn.net/v/t1.0-0/p180x540/57485234_580372475773737_4588144078612332544_o.jpg?_nc_cat=103&ccb=2&_nc_sid=8bfeb9&_nc_ohc=IV43Rgy39v4AX9-ZSWL&_nc_oc=AQmZvfhkZdvMJiLbg0Fj6lRgOHgXw04KPyv1cW55LJl8SqeOszueaOs2ejAt-Y76NSM&_nc_ht=scontent.fblr4-1.fna&tp=6&oh=565242f1befbb81be25fe1e205b340c3&oe=5FC0CCD1",
+  },
+  {
+    id: 5,
+    name: "J.Rahul Sainath",
+    age: "28th October 2003",
+    image:
+      "https://scontent.fblr4-3.fna.fbcdn.net/v/t1.0-9/122760335_370228824053175_7134486283766095637_o.jpg?_nc_cat=100&ccb=2&_nc_sid=730e14&_nc_ohc=TOr3XC2dQHkAX-wsEIe&_nc_oc=AQlIi31zFyu4BSUBZSMXfv8rQ0tBvsopTHxA-j1kvVrf8BTVKvL4MJramBx2V8H2JpY&_nc_ht=scontent.fblr4-3.fna&oh=5cda3e5d8af6be11f191ec7fee68581d&oe=5FC1D5EB",
+  },
+  {
+    id: 6,
+    name: "Prasanna",
+    age: "29th May 2004",
+    image:
+      "https://images-platform.99static.com/jQu2xohritutSVmnVq7np7rbkxg=/0x0:1920x1920/500x500/top/smart/99designs-contests-attachments/106/106359/attachment_106359975",
+  },
+  {
+    id: 7,
+    name: "Sindhu",
+    age: "26th February",
+    image:
+      "https://cdn2.iconfinder.com/data/icons/avatar-colorize-i/100/people_character_avatar_smile_1-16-512.png",
+  },
+  {
+    id: 8,
+    name: "Abhishek",
+    age: "14th March",
+    image:
+      "https://scontent.fblr4-3.fna.fbcdn.net/v/t1.0-0/p180x540/123260496_186397346402668_6660943545537601833_o.jpg?_nc_cat=106&ccb=2&_nc_sid=8bfeb9&_nc_ohc=vbvBCfufRa0AX92ZyQ2&_nc_ht=scontent.fblr4-3.fna&tp=6&oh=5a2775d7d973c26346188f4d442a1ed3&oe=5FC0F647",
+  },
+  {
+    id: 9,
+    name: "Chidvilash",
+    age: "9th September 2004",
+    image:
+      "https://scontent.fblr4-3.fna.fbcdn.net/v/t1.0-9/118893072_785263332273621_4098774270814909783_n.jpg?_nc_cat=106&ccb=2&_nc_sid=09cbfe&_nc_ohc=tTpqPPfUd-kAX8zO47R&_nc_ht=scontent.fblr4-3.fna&oh=6f8178482b2fc708e8c646353145d2b5&oe=5FC1D113",
+  },
+  {
+    id: 10,
+    name: "Keshavanth",
+    age: "May 23",
+    image:
+      "https://www.kindpng.com/picc/m/136-1369892_avatar-people-person-business-user-man-character-avatar.png",
+  },
+  {
+    id: 11,
+    name: "Arif",
+    age: "April 10",
+    image:
+      "https://scontent.fblr4-2.fna.fbcdn.net/v/t1.0-0/s526x395/96763544_1150530501964287_2788169021965467648_n.jpg?_nc_cat=107&ccb=2&_nc_sid=174925&_nc_ohc=2-w7zPz1b30AX_oC9MP&_nc_ht=scontent.fblr4-2.fna&tp=7&oh=cdc5398ac71b65b1cab0626d67d65597&oe=5FC2665D",
+  },
+  {
+    id: 12,
+    name: "Puneeth",
+    age: "17th December",
+    image:
+      "https://scontent.fblr4-3.fna.fbcdn.net/v/t1.0-0/c0.0.720.720a/s526x395/46268082_488713751614820_6854726203362770944_n.jpg?_nc_cat=100&ccb=2&_nc_sid=174925&_nc_ohc=Q_0z2c-dYH0AX9kJHXC&_nc_ht=scontent.fblr4-3.fna&_nc_tp=28&oh=bd487cb4a4abd8ca1f6b4b44bab64bda&oe=5FC08926",
+  },
+];
